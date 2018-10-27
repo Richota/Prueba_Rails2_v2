a0f9606d28f2de1555f3865f2b7a2ffbf1ef41c0
Ricardo Suarez <rsuarez.adv@gmail.com>
2018-10-27 03:26:47 -0700

    WIP: aun nadagit add .

--- ToDoList2/app/controllers/tasks_controller.rb
+++ ToDoList2/app/controllers/tasks_controller.rb
@@ -1,11 +1,14 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!
   def index
     @tasks = Task.all
     @user = current_user
-    @ready = @ready = Ready.new(task: @task, user: current_user)
+    #@ready = Ready.new(task: @task, user: current_user)
+    #@ready = Ready.all.where(state: true)
+    @ready = Ready.all.where(user_id: current_user)
   end
 
-  def method_name
+  def show
     @task = Task.find(params[:id])
   end
 end
